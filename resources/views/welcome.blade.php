fa023126a18412ab7f696921f0c6b43cb857cc7a
Edgar Rivas <earivas@gmail.com>
2019-08-31 22:21:09 -0500

    Commit
    Primer Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel con Vue
+                    Laravel con Vue.js
                 </div>
 
                 <div class="links">
